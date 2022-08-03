0c8d5a2c47f30a9b97bc96b526081003f1ba2ff9
Furkan <106157697+MacroKeke@users.noreply.github.com>
2022-08-03 18:08:03 +0300

    Add files via upload

--- handlers/admins.py
+++ handlers/admins.py
@@ -147,5 +147,5 @@ async def update_admin(client, message):
     admins[message.chat.id] = new_admins
     await client.send_message(
         message.chat.id,
-        "✅ **Bot yeniden başladı!**\n✅ **Admin listesi güncellendi!** \n **Emek Var Kardeşim Emek Geliştirici @MacroPem**"
+        "✅ **Bot yeniden başladı!**\n✅ **Admin listesi güncellendi!** \n\n **ANCA MEZARDA USLANIRSINIZ!**"
     )
